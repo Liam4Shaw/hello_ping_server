461be7663643496d2434533677b8df1df8da3cec
Liam4Shaw <liamshaw421@gmail.com>
2024-06-26 12:55:04 +0530

    added test cases for route_lookup

--- test_hello_ping_server.py
+++ test_hello_ping_server.py
@@ -1,7 +1,39 @@
 import unittest
-from hello_ping_server import handle_root, handle_hello, handle_ping, handle_404, handle_bad_request, handle_access_denied, handle_error
+from hello_ping_server import route_lookup, handle_root, handle_hello, handle_ping, handle_404, handle_bad_request, handle_access_denied, handle_error
 
-class TestHTTPHandlers(unittest.TestCase):
+class TestRouteLookup(unittest.TestCase):
+    
+    def setUp(self):
+        self.request = {'path': '', 'method': 'GET'}
+
+    def test_route_lookup_root(self):
+        self.request['path'] = '/'
+        self.assertEqual(route_lookup(self.request), handle_root)
+        # For POST instead of expected GET request, 404 error is raised
+        self.request['method'] = 'POST'
+        self.assertEqual(route_lookup(self.request), handle_404)
+    
+    def test_route_lookup_hello(self):
+        self.request['path'] = '/hello'
+        self.assertEqual(route_lookup(self.request), handle_hello)
+
+    def test_route_lookup_ping(self):
+        self.request['path'] = '/ping'
+        self.assertEqual(route_lookup(self.request), handle_ping)
+
+    def test_route_lookup_404(self):
+        self.request['path'] = '/nope'
+        self.assertEqual(route_lookup(self.request), handle_404)
+
+    def test_route_lookup_badrequest(self):
+        self.request['path'] = '/badrequest'
+        self.assertEqual(route_lookup(self.request), handle_bad_request)
+
+    def test_route_lookup_accessdenied(self):
+        self.request['path'] = '/accessdenied'
+        self.assertEqual(route_lookup(self.request), handle_access_denied)
+        
+class TestHelloPingServer(unittest.TestCase):
 
     def setUp(self):
         self.request = {'method': 'GET', 'headers': {}}
